5a03c445f618aad29d9c51514b9d766c02428364
vrnpln <voronetskayapolina5@gmail.com>
2025-01-01 22:23:00 +1000

    Add files via upload
    Added all CRUD methods (Create, Read, Update, Delete)

--- main.go
+++ main.go
@@ -27,6 +27,38 @@ func GetMessages(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(messages)
 }
 
+func UpdateMessages(w http.ResponseWriter, r *http.Request) {
+	var message Message
+
+	// Получение переменных из маршрута
+	vars:= mux.Vars(r)
+	// Получение ID из переменных маршрута
+	id := vars["id"]
+	// Поиск задачи по ID
+	DB.First(&message,id)
+
+	// Кодирирование структуры Message в JSON и отправка
+	json.NewDecoder(r.Body).Decode(&message)
+	// Сохранение изменений в БД
+	DB.Save(&message)
+	// Кодирование в JSON и отправка обновленной задачи
+	json.NewEncoder(w).Encode(message)
+}
+
+func DeleteMessages( w http.ResponseWriter, r *http.Request) {
+	var message Message
+
+	// Получение переменных из маршрута
+	vars:= mux.Vars(r)
+	// Получение ID из переменных маршрута
+	id := vars["id"]
+	// Поиск задачи по ID
+	DB.First(&message,id)
+
+	// Удаление найденной задачи в БД
+	DB.Delete(&message)
+}
+
 func main() {
 	// Вызываем метод InitDB() из файла db.go
 	InitDB()
@@ -37,5 +69,7 @@ func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/api/messages", CreateMessage).Methods("POST")
 	router.HandleFunc("/api/messages", GetMessages).Methods("GET")
+	router.HandleFunc("/api/messages/{id}", UpdateMessages).Methods("PATCH")
+	router.HandleFunc("/api/messages/{id}", DeleteMessages).Methods("DELETE")
 	http.ListenAndServe(":8080", router)
 }
